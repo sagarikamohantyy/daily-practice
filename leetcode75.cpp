ff4591f5b62ffd685fb7374ceaaed2aa68f8099a
sagarikamohantyy <sagarika0927@gmail.com>
2026-01-29 23:59:17 +0530

    practice

---
+++ leetcode75.cpp
@@ -0,0 +1,44 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int low=0, mid=0, high=nums.size()-1;
+        while(mid<=high){
+            if(nums[mid]==0){
+                swap(nums[low], nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==2){
+                swap(nums[mid], nums[high]);
+                high--;
+            }
+            else if(nums[mid]==1){
+                mid++;
+            }
+        }
+    }
+};
+
+int main() {
+    cout << "Enter no. of elements:";
+    int n;
+    cin >> n;
+    cout << "Enter elements (0s, 1s, and 2s):";
+    vector<int> nums(n);
+    for(int i=0; i<n; i++) {
+        cin >> nums[i];
+    }
+
+    Solution obj;
+    obj.sortColors(nums);
+
+    for(int i=0; i<n; i++){
+        cout<<nums[i];
+    }
+    return 0;
+}
